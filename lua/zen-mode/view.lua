6f5702db4fd4a4c9a212f8de3b7b982f3d93b03c
Matt Palmer <490790+MattSPalmer@users.noreply.github.com>
2022-09-05 04:56:52 -0400

    feat: improved tmux integration (#48)
    * Hide pane status, respect existing status state
    
    Anyone wanting to hide their tmux UI will also want their pane border title hidden as well.
    
    Both the pane status and main status lines may have existing settings prior to launching zen-mode. Preserve those and restore them when zen-mode closes.
    
    * Re-establish window width and height in fix_layout
    
    * Use existing layout implementation

--- lua/zen-mode/view.lua
+++ lua/zen-mode/view.lua
@@ -126,6 +126,8 @@ end
 function M.fix_layout(win_resized)
   if M.is_open() then
     if win_resized then
+      local l = M.layout(M.opts)
+      vim.api.nvim_win_set_config(M.win, { width = l.width, height = l.height })
       vim.api.nvim_win_set_config(M.bg_win, { width = vim.o.columns, height = M.height() })
     end
     local height = vim.api.nvim_win_get_height(M.win)
