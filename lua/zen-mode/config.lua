8b95163ae0782a326d252a64c0bae60e4b45506a
Max Rossmannek <max.rossmannek@uzh.ch>
2023-04-17 19:41:18 +0200

    Update wezterm integration docs

--- lua/zen-mode/config.lua
+++ lua/zen-mode/config.lua
@@ -51,31 +51,7 @@ local defaults = {
       font = "14", -- font size
     },
     -- this will change the font size on wezterm when in zen mode
-    -- to make this work, you need to set the following function in your wezterm
-    -- config:
-    -- wezterm.on('user-var-changed', function(window, pane, name, value)
-    --     local overrides = window:get_config_overrides() or {}
-    --     if name == "ZEN_MODE" then
-    --         local incremental = value:find("+")
-    --         local number_value = tonumber(value)
-    --         if incremental ~= nil then
-    --             while (number_value > 0) do
-    --                 window:perform_action(wezterm.action.IncreaseFontSize, pane)
-    --                 number_value = number_value - 1
-    --             end
-    --             overrides.enable_tab_bar = false
-    --         elseif number_value < 0 then
-    --             window:perform_action(wezterm.action.ResetFontSize, pane)
-    --             overrides.font_size = nil
-    --             overrides.enable_tab_bar = true
-    --         else
-    --             overrides.font_size = number_value
-    --             overrides.enable_tab_bar = false
-    --         end
-    --     end
-    --     window:set_config_overrides(overrides)
-    -- end)
-    -- See also: https://github.com/wez/wezterm/discussions/2550
+    -- See alse also the Plugins/Wezterm section in this projects README
     wezterm = {
       enabled = false,
       -- can be either an absolute font size or the number of incremental steps
