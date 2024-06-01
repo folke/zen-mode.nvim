15ae79c22c371563976f71b4ae7cc754dd7aa534
Eduard Korchmar <korchmar.eduard@gmail.com>
2024-06-01 22:20:52 +0200

    feat(neovide): implement scaling and reset

--- lua/zen-mode/plugins.lua
+++ lua/zen-mode/plugins.lua
@@ -123,6 +123,22 @@ function M.tmux(state, disable, opts)
   end
 end
 
+function M.neovide(state, disable, opts)
+  if not vim.g.neovide then
+    return
+  end
+  if disable then
+    if opts.scale ~= 1 then
+      state.scale = vim.g.neovide_scale_factor
+      vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * opts.scale
+    end
+  else
+    if opts.scale ~= 1 then
+      vim.g.neovide_scale_factor = state.scale
+    end
+  end
+end
+
 function M.diagnostics(state, disable)
   if disable then
     vim.diagnostic.disable(0)
